0e5588524285aa8b3f2073602a7a6d0154996a6f
Amr Ez Eldin Elsayed Rashed <amrrashed200@gmail.com>
2020-02-22 18:26:41 +0300

    Delete test.m

--- test.m
+++
@@ -1,9 +0,0 @@
-load simpleDL.mat trainedNet lgraph
-digitDatasetPath = fullfile('C:\Users\amr rashed\Desktop\Matlab\ADBTEST');
-imds = imageDatastore(digitDatasetPath, ...
-    'IncludeSubfolders',true,'LabelSource','foldernames');
-img = readimage(imds,100);
-actualLabel = imds.Labels(100);
-predictedLabel = trainedNet.classify(img);
-imshow(img);
-title(['Predicted: ' char(predictedLabel) ', Actual: ' char(actualLabel)])
